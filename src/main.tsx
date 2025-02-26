a4caa4a927aad52261fabd187c30c3f462a8a005
destinydaniel098689 <destinydaniel098689@gmail.com>
2025-02-26 20:20:36 +0100

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router'
 import Instagram from './components/Instagram.tsx'
 import Facebook from './components/Facebook.tsx'
 
@@ -10,9 +10,12 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<App />} />
-        <Route path="instagram" element={<Instagram />} />
-        <Route path="facebook" element={<Facebook />} />
+        <Route path="/" element={<Outlet />}>
+          <Route index element={<App />} />
+          <Route path="instagram" element={<Instagram />} />
+          <Route path="facebook" element={<Facebook />} />
+        </Route>
+
         <Route path='*' element={<App />} />
       </Routes>
     </BrowserRouter>
